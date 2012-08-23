49da0a0925f602bb9674b09becf0bd79c837d685
Innocenty Enikeew <enikesha@gmail.com>
2012-08-23 22:16:28 +0400

    Use south

--- dj_todo/settings.py
+++ dj_todo/settings.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'gunicorn',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
